591fb11d0c5168e33de97341ce9d16cf7009f6e2
tauhid234 <syifatauhid@gmail.com>
2021-03-21 09:49:16 +0700

    APPLY API KEY IN GOODS CONTROLLER

--- src/main/java/com/backend/dlhapi/controller/GoodsController.java
+++ src/main/java/com/backend/dlhapi/controller/GoodsController.java
@@ -3,7 +3,9 @@ package com.backend.dlhapi.controller;
 import com.backend.dlhapi.model.GoodsModel;
 import com.backend.dlhapi.resource.MessageResponse;
 import com.backend.dlhapi.service.GoodsService;
+import com.backend.dlhapi.service.SessionService;
 import java.util.Collection;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -23,25 +25,42 @@ public class GoodsController {
     @Autowired
     private GoodsService svc;
     
+    @Autowired
+    private SessionService service;
+    
     @RequestMapping(path = "insert_goods", method = RequestMethod.POST)
-    public ResponseEntity insertOfGoods(@RequestParam("name") String name, @RequestParam("date") String date, @RequestParam("amount") String amount, GoodsModel model){
+    public ResponseEntity insertOfGoods(@RequestParam("api_key") String api_key, @RequestParam("name") String name, @RequestParam("date") String date, @RequestParam("amount") String amount, GoodsModel model){
         
         if(name.equals("")){
             return new MessageResponse().BadRequest();
         }
-        model.setName(name);
-        model.setDate(date);
-        model.setAmount(amount);
         
-        
-        svc.insertGoods(model);
-        return new MessageResponse().Succes();
+        Optional data = service.ApiKeySet(api_key);
+        Collection data2 = svc.getDataGoods();
+        if(data.isPresent()){
+            if(!data2.isEmpty()){
+                model.setName(name);
+                model.setDate(date);
+                model.setAmount(amount);
+                
+                svc.insertGoods(model);
+                return new MessageResponse().Succes();
+            }
+        }
+        return new MessageResponse().NotFound();
     }
     
     @GetMapping("goods_data")
-    public ResponseEntity getAllGoods(){
-        HttpHeaders header = new HttpHeaders();
-        header.add("Content-Type", "application/json; charset=utf-8");
-        return new ResponseEntity(svc.getDataGoods(), header, HttpStatus.OK);
+    public ResponseEntity getAllGoods(@RequestParam("api_key") String api_key){
+        Collection data = svc.getDataGoods();
+        Optional data2 = service.ApiKeySet(api_key);
+        if(data2.isPresent()){
+            if(!data.isEmpty()){
+                HttpHeaders header = new HttpHeaders();
+                header.add("Content-Type", "application/json; charset=utf-8");
+                return new ResponseEntity(svc.getDataGoods(), header, HttpStatus.OK);
+            }
+        }
+        return new MessageResponse().NotFound();
     }
 }
